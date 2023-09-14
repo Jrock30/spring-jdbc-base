8914348803cd2cb1c2c9078323c4105b94cf9d5c
jrock30 <jrg8989@knou.ac.kr>
2023-09-15 03:57:04 +0900

    feat: end

--- src/main/java/jrock/jdbc/service/MemberServiceV3_3.java
+++ src/main/java/jrock/jdbc/service/MemberServiceV3_3.java
@@ -2,37 +2,27 @@ package jrock.jdbc.service;
 
 import jrock.jdbc.domain.Member;
 import jrock.jdbc.repository.MemberRepositoryV3;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
 /**
- * 트랜잭션 - 트랜잭션 매니저
+ * 트랜잭션 - @Transactional AOP
  */
 @Slf4j
-@RequiredArgsConstructor
-public class MemberServiceV3_1 {
+public class MemberServiceV3_3 {
 
-    private final PlatformTransactionManager transactionManager;
     private final MemberRepositoryV3 memberRepository;
 
+    public MemberServiceV3_3(MemberRepositoryV3 memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Transactional
     // 계좌 이체
     public void accountTransfer(String fromId, String toId, int money) throws SQLException {
-        // 트랜잭션 시작
-        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
-
-        try {
-            // 비즈니스 로직
-            bizLogic(fromId, toId, money);
-            transactionManager.commit(status); //  성공시 커밋
-        } catch (Exception e) {
-            transactionManager.rollback(status); // 실패 시 롤백
-            throw new IllegalStateException(e);
-        }
+        bizLogic(fromId, toId, money);
     }
 
     private void bizLogic(String fromId, String toId, int money) throws SQLException {
